937711f554d70eb00594934e99f46187e4e3a866
jouiwnl <jou.098olo@gmail.com>
2021-12-17 19:49:02 -0300

    corrige validação prefix

--- src/utils/checkCommand.js
+++ src/utils/checkCommand.js
@@ -1,15 +1,21 @@
 import prefix from './prefix.js';
 
 const getCommand = (message) => {
+  if (message.content.startsWith(prefix)) {
+    return;
+  }
   const commandBody = message.content.slice(prefix.length);
   const argumentos = commandBody.split(' ');
   const command = argumentos.shift().toLowerCase();
-
 
   return command;
 };
 
 const getArgs = (message) => {
+  if (message.content.startsWith(prefix)) {
+    return;
+  }
+
   const commandBody = message.content.slice(prefix.length);
   const arg = commandBody.split(' ');
   // eslint-disable-next-line no-unused-vars
